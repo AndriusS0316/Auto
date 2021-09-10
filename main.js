ab1fa05a2b877bb5973d467721046747d2ab8516
Andrius <andriuss0316@gmail.com>
2021-09-10 09:51:20 +0300

    20210910

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
 
 
 
-let klausimas1;
+let klausimas1 = '';
+let klausimas2 = '';
+let klausimas3 = '';
+let klausimas4 = '';
+let klausimas5 = '';
+let klausimas6 = '';
+let klausimas7 = '';
+let klausimas8 = '';
+let klausimas9 = '';
+let klausimas10 = '';
+
+let bonusKlausimas = '';
 
 
 const start = document.querySelector('.start'); 
