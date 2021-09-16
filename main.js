73238921fcf61a2ed1514fc9772898623915e839
Andrius <andriuss0316@gmail.com>
2021-09-16 14:55:30 +0300

    20210916

--- main.js
+++ main.js
@@ -4317,26 +4317,6 @@ function bonusClick() {
 
 
 
-// Run myfunc every second
-var myfunc = setInterval(function () {
-
-    var now = new Date().getTime();
-    var timeleft = timeleft - now;
-
-    // Calculating the days, hours, minutes and seconds left
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-    // Result is output to the specific element
-    document.getElementById("sec").innerHTML = seconds + "s "
-
-    // Display the message when countdown is over
-    if (timeleft < 0) {
-        clearInterval(myfunc);
-        document.getElementById("sec").innerHTML = ""
-        document.getElementById("end").innerHTML = "TIME UP!!";
-    }
-}, 1000);
-
 
 
 start.addEventListener('click', startClick);
